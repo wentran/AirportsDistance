82d4345db2aa5aeabbf0f214d9920645100e8ef2
wentran <wen.tran1@gmail.com>
2016-10-24 00:28:46 -0400

    buttons

--- javascript/distancePlot.js
+++ javascript/distancePlot.js
@@ -1,5 +1,5 @@
 /**
- * PURPOSE: Display Google Maps on screen - powered by Google API
+Display Google Map on screen - utilized Google Map API
  */
 
 function initialize(startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong) {
@@ -15,7 +15,7 @@ function initialize(startAirportLat, startAirportLong, arrivingAirportLat, arriv
 // Define the custom symbols. All symbols are defined via SVG path notation.
 // They have varying stroke color, fill color, stroke weight,
 // opacity and rotation properties.
-  var symbolOne = {
+  var startAirportMarker = {
            path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
            fillColor: '#FF0000',
            fillOpacity: .6,
@@ -24,7 +24,7 @@ function initialize(startAirportLat, startAirportLong, arrivingAirportLat, arriv
            scale: 0.25
 
   }
-  var symbolThree = {
+  var arrivingAirportMarker = {
            path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
            fillColor: '#FF0000',
            fillOpacity: .6,
@@ -45,55 +45,17 @@ function initialize(startAirportLat, startAirportLong, arrivingAirportLat, arriv
     path: lineCoordinates,
     icons: [
       {
-        icon: symbolOne,
+        icon: startAirportMarker,
         offset: '0%'
       },  
       {
-        icon: symbolThree,
+        icon: arrivingAirportMarker,
         offset: '100%'
       }
     ],
     map: map
   });
 
-}
-
-/**
- * FILE NAME: GetDistance.js
- * PURPOSE: Validate and get distance between two airport inputs.
- * SIDE EFFECT: Initializes Google Map after successfully displaying distance.
- */
-
-function openGoogleMaps (startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong){
-  initialize(startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong)
-}
-
-function getArrivingAirportLongLat (arrivingAirport, key, startAirportLat, startAirportLong) {
-  $.ajax({
-    type: 'GET',
-    url : "https://airport.api.aero/airport/" + arrivingAirport + "?user_key=" + key,
-    dataType : "jsonp",
-    success : function(parsed_json) {
-      arrivingAirportLat = parsed_json.airports[0].lat;
-      arrivingAirportLong = parsed_json.airports[0].lng;
-       
-      openGoogleMaps (startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong);
-     }
-  });
-}
-
-function getStartingAirportLongLat (startAirport, arrivingAirport, key) {
-  $.ajax({
-    type: 'GET',
-    url : "https://airport.api.aero/airport/" + startAirport + "?user_key=" + key,
-    dataType : "jsonp",
-    success : function(parsed_json) {
-      startAirportLat = parsed_json.airports[0].lat;
-      startAirportLong = parsed_json.airports[0].lng;
-      
-      getArrivingAirportLongLat(arrivingAirport, key, startAirportLat, startAirportLong);
-    }
-  });
 }
 
 
@@ -128,16 +90,10 @@ $(document).ready(function(){
         // openGoogleMaps (startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong);
         initialize (startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong)
         // initialize(40.639751, 73.778925, 33.942536,118.408075)
-      }
-
-
-    })
+                }
+              })
+            }
         }
-
-      }
-    // get arriving airport longLat
-
-  });
-
+      });
   });
 });
