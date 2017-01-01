0e46b3e2e622f5427a7375f9b84e8971a525a16e
wentran <wen.tran1@gmail.com>
2017-01-01 18:58:19 -0500

    distance updates

--- client/javascript/getDistance.js
+++ client/javascript/getDistance.js
@@ -8,39 +8,55 @@ $(document).ready(function() {
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    result = (IntentMedia.distance_between_airports(startAirport,arrivingAirport )) + " " +"miles"
-    var route = startAirport + " "+ 'to'+ " "+ arrivingAirport;
-    cell1.innerHTML = route;
-    cell2.innerHTML = result;
-
 
 // ajax calls and plot route on map
 var apiKey = '5893b60d8bde3259e625ced3de9ef69f';
-$.ajax({
-  type: 'GET',
-  url : "https://airport.api.aero/airport/" + startAirport + "?user_key=" + apiKey,
-  dataType : "jsonp",
-  success : function(result) {
-    startAirportLat = result.airports[0].lat;
-    startAirportLong = result.airports[0].lng;
-    console.log(startAirportLat,startAirportLong)
-            if (startAirport === false)
-          $("#messageErr").html("please pick a different arriving airport")
-        else {
-          $.ajax({
-            type: 'GET',
-            url : "https://airport.api.aero/airport/" + arrivingAirport + "?user_key=" + apiKey,
-            dataType : "jsonp",
-            success : function(result) {
-              arrivingAirportLat = result.airports[0].lat;
-              arrivingAirportLong =result.airports[0].lng;
-              // console.log(startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong)
+$.when (
+  $.ajax({
+    type: 'GET',
+    url : "https://airport.api.aero/airport/" + startAirport + "?user_key=" + apiKey,
+    dataType : "jsonp",
+    success : function(result) {
+      startAirportLat = result.airports[0].lat;
+      startAirportLong = result.airports[0].lng;
+      console.log(startAirportLat,startAirportLong)
+      if (startAirport === false)
+        $("#messageErr").html("please pick a different arriving airport")
+      else {
+        $.ajax({
+          type: 'GET',
+          url : "https://airport.api.aero/airport/" + arrivingAirport + "?user_key=" + apiKey,
+          dataType : "jsonp",
+          success : function(result) {
+            arrivingAirportLat = result.airports[0].lat;
+            arrivingAirportLong =result.airports[0].lng;
+                // console.log(startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong)
                 initialize (startAirportLat, startAirportLong, arrivingAirportLat, arrivingAirportLong)
-                    }
-                })
-            }
-          }
-      });
-    });
+              }
+            })
+      }
+    }
+  }),
+  // https://airport.api.aero/airport/distance/jfk/bna?user_key=5893b60d8bde3259e625ced3de9ef69f
+  // startAirport = startAirport.toLowerCase();
+  // arrivingAirport = arrivingAirport.toLowerCase();
+  $.ajax({
+    type: 'GET',
+    url : "https://airport.api.aero/airport/distance/" + startAirport.toLowerCase() + "/" + arrivingAirport.toLowerCase() + "?user_key=" + apiKey,
+    dataType : "jsonp",
+    success : function(resultDistance) {
+      console.log('clicking ----distance api body---',resultDistance.distance)
+      var distanceResult = resultDistance.distance + " " +"km"
+      var route = startAirport + " "+ 'to'+ " "+ arrivingAirport;
+      cell1.innerHTML = route;
+      cell2.innerHTML = distanceResult;
+    }
+  })
+  ).then (function (){
+    console.log('hereeee')
+  })
+
+
+});
 });
 
