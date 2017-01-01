a34ca7bda4879b3a2a0b00155a600d6fc9448d30
Wen Tran <wen.tran1@gmail.com>
2017-01-01 13:08:54 -0500

    Update flights.js

--- client/javascript/flights.js
+++ client/javascript/flights.js
@@ -1,39 +1,28 @@
-var IntentMedia = IntentMedia || {};
-
-IntentMedia.Airports = (function () {
-    var pub = {};
-
-    pub.airport_exists = function (airport_code) {
-      return pub.airport_distances().hasOwnProperty(airport_code);
-    };
-
-    pub.airport_distances = function () {
-      return {
-        JFK: {LAX: 2475, LAS: 2248, PDX: 2454},
-        LAX: {JFK: 2475, LAS: 236, PDX: 834},
-        LAS: {JFK: 2248, LAX: 236, PDX: 763},
-        PDX: {JFK: 2454, LAS: 763, LAX: 834}
+var IntentMedia = (function () {
+  var airport_distances = airport_distances || airport_map();
+  function airport_map () {
+    return {
+      JFK: {LAX: 2475, LAS: 2248, PDX: 2454},
+      LAX: {JFK: 2475, LAS: 236, PDX: 834},
+      LAS: {JFK: 2248, LAX: 236, PDX: 763},
+      PDX: {JFK: 2454, LAS: 763, LAX: 834}
+    }
+  }
+  function airport_exists (airport_code) {
+    return airport_map().hasOwnProperty(airport_code);
+  }
+  function distance_between_airports (from_airport, to_airport) {
+    if (airport_exists(from_airport) &&airport_exists(to_airport)) {
+      if (from_airport === to_airport) {
+        return 0;
       }
-    };
-
-    return pub;
-}(IntentMedia || {}));
-
-IntentMedia.Distances = (function () {
-    var pub = {};
-    var airport_distances = airport_distances || IntentMedia.Airports.airport_distances();
-
-    pub.distance_between_airports = function (from_airport, to_airport) {
-      if (IntentMedia.Airports.airport_exists(from_airport) && IntentMedia.Airports.airport_exists(to_airport)) {
-        if (from_airport === to_airport) {
-          return 0;
-        }
-
-        return airport_distances[from_airport][to_airport];
-      }
-
-      return -1;
-    };
-
-    return pub;
-}(IntentMedia || {}));
+      return airport_distances[from_airport][to_airport];
+    }
+    return -1;
+  }
+  return {
+    airport_map: airport_map,
+    airport_exists: airport_exists,
+    distance_between_airports: distance_between_airports
+  }    
+})();
