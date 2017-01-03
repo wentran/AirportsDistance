94e975313ac441e00bec42e00720f860f3976884
wentran <wen.tran1@gmail.com>
2017-01-02 23:24:44 -0500

    auto complete semiworking

---
+++ client/javascript/autocomplete.js
@@ -0,0 +1,22 @@
+// var options = {
+//     url: "data/airports.json",
+
+//     getValue: "name",
+
+//     template: {
+//         type: "description",
+//         fields: {
+//             description: "code"
+//         }
+//     },
+
+//     list: {
+//         match: {
+//             enabled: true
+//         }
+//     },
+
+//     theme: "plate-dark"
+// };
+
+// $("#example-mail").easyAutocomplete(options);
